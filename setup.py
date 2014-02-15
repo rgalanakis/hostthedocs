0b0422073b66b4ee0538d2a00865f2c59a9dc37b
Rob Galanakis <rob.galanakis@gmail.com>
2014-02-15 14:00:48 +0000

    Added setup.py and manifest.in

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='hostthedocs',
+      version='0.1.0',
+      description='Makes documentation hosting easy.',
+      author='Rob Galanakis',
+      author_email='rob.galanakis@gmail.com',
+      url='https://github.com/rgalanakis/hostthedocs',
+      packages=['hostthedocs'],
+     )
