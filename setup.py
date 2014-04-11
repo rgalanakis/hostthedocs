09b62a879ba4d63b3ca0cb5c320337ad94304bc6
Rob Galanakis <rob.galanakis@gmail.com>
2014-04-11 11:17:52 -0400

    Deleted requirements files, use setup.py instead. Fixed tox deps

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='hostthedocs',
-      version='0.2.0',
-      description='Makes documentation hosting easy.',
-      author='Rob Galanakis',
-      author_email='rob.galanakis@gmail.com',
-      url='https://github.com/rgalanakis/hostthedocs',
-     )
+setup(
+    name='hostthedocs',
+    version='0.2.0',
+    description='Makes documentation hosting easy.',
+    author='Rob Galanakis',
+    author_email='rob.galanakis@gmail.com',
+    url='https://github.com/rgalanakis/hostthedocs',
+    packages=find_packages(),
+    install_requires=['Flask'],
+)
